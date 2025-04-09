8536bb6d6ea9ef673c5067f5e934e30578e63875
Randall Marino <rmarino2511@gmail.com>
2025-04-08 22:46:09 -0600

    Cambios

--- LoRa_APRS_Tracker-main/src/utils.cpp
+++ LoRa_APRS_Tracker-main/src/utils.cpp
@@ -97,7 +97,7 @@ namespace Utils {
             uint32_t statusTx = currentTime - statusTime;
             lastTx = currentTime - lastTxTime;
             if (statusTx > 10 * 60 * 1000 && lastTx > 10 * 1000) {
-                LoRa_Utils::sendNewPacket(APRSPacketLib::generateStatusPacket(currentBeacon->callsign, "APLRT1", Config.path, "¡Hola, somos el grupo 2!" + versionDate));
+                LoRa_Utils::sendNewPacket(APRSPacketLib::generateStatusPacket(currentBeacon->callsign, "APLRT1", Config.path, "https://github.com/Takita7/APRS-LORA.git " + versionDate));
                 statusState = false;
             }
         }
